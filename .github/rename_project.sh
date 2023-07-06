35abc34c6dd5855a2e8182868402d0d0d782df6f
sathishkumartheta <sathishkumartheta@users.noreply.github.com>
2023-07-06 10:43:03 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="sathishkumartheta"
+original_name="iris_matching"
+original_urlname="iris-matching"
+original_description="Awesome iris_matching created by sathishkumartheta"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv iris_matching $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from iris_matching.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "iris_matching"
